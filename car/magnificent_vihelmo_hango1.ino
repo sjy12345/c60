59f781f182d669b6f1ea1ea685d39b05c2a0a59f
sjy12345 <52559605+sjy12345@users.noreply.github.com>
2019-07-05 16:46:42 +0800

    Add files via upload

--- car/magnificent_vihelmo_hango1.ino
+++ car/magnificent_vihelmo_hango1.ino
@@ -1,2 +1,86 @@
-int IN1=4;int IN2=5;int IN3=6;int IN4=7; int EN1=10;//使能端口1int EN2=11;//使能端口2//数字端口4与5为一组，6与7为另一组//数字端口10 11 作为直流电机的使能端void Motor1(boolean reverse)//电机1正转{  if (reverse) //正转  {    digitalWrite(IN1,HIGH);    digitalWrite(IN2,LOW);  }   else //反转  {    digitalWrite(IN1,LOW);    digitalWrite(IN2,HIGH);  } } void Motor2(boolean reverse)//电机2转动{  if (reverse) //正转  {    digitalWrite(IN3,HIGH);    digitalWrite(IN4,LOW);  }   else //反转  {    digitalWrite(IN3,LOW);    digitalWrite(IN4,HIGH);  } } void aa()//先正转1秒停1秒后反转1秒{    //正转  digitalWrite(IN3,LOW);  digitalWrite(IN4,HIGH);  digitalWrite(IN1,LOW);  digitalWrite(IN2,HIGH);   delay(1000);   //暂停一秒  stopMotor(true);  delay(1000);   stopMotor(false);//恢复电机使能端高电平    //反转  digitalWrite(IN3,HIGH);  digitalWrite(IN4,LOW);  digitalWrite(IN1,HIGH);  digitalWrite(IN2,LOW);    delay(1000);  // 暂停  stopMotor(true);  delay(1000);   stopMotor(false);   } void stopMotor(boolean r)//电机停转{  if (r)  {    analogWrite(EN1,0);    analogWrite(EN2,0);  }  else  {    analogWrite(EN1,255);     analogWrite(EN2,255);  }}  void setup(){  int i;    for (i=4;i<=7;i++) //为Ardunio 电机驱动板    pinMode(i,OUTPUT); //设置数字端口4,5,6,7为输出模式   for (i=4;i<=7;i++)     digitalWrite(i,OUTPUT); //设置数字端口4,5,6,7为HIGH,电机保持不动    pinMode(10,OUTPUT);//设置数字端口10  11为输出模式  pinMode(11,OUTPUT);  Serial.begin(9600); } void loop(){  aa();//两个直流电机正转1秒 暂停一秒 反转一秒 }
+#include"Arduino.h"
+@#include"Smorse.h"
+{
+  	pinMode(int  pin)
+      _pin=pin;
+  -dottime=250;
+  
+}
 
+void SMorse::dot()
+{
+digitalWrite(_pin,HIGH);
+delay(_dottime);
+digitalWrite(_pin,LOW);
+delay(_dottime);
+}
+
+void SMorse::dash()
+{
+digitalWrite(_pin,HIGH);
+delay(_dottime*4);
+digitalWrite(_pin,LOW);
+delay(_dottime);
+}
+
+void SMorse::c_space()
+{
+digitalWrite(_pin,LOW);
+delay(_dottime*3);
+}
+
+void SMorse::w_space()
+{
+digitalWrite(_pin,LOW);
+delay(_dottime*7);
+}
+void SMorse::al_a()
+{
+dot();
+dash();
+}
+void SMorse::al_b()
+{
+dash();
+dot();
+dot();
+dot();
+}
+void SMorse::al_c()
+{
+dash();
+dot();
+dash();
+dot();
+}
+void SMorse::al_d()
+{
+dash();
+dot();
+dot();
+}
+void SMorse::al_e()
+{
+dot();
+}
+void SMorse::al_f()
+{
+dot();
+dot();
+dash();
+dot();
+}
+void SMorse::al_g()
+{
+dash();
+dash();
+dot();
+}
+void SMorse::al_h()
+{
+dot();
+dot();
+dot();
+dot();
+dot();
+}
